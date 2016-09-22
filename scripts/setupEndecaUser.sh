b00dc9296d2ad4962d75ee2e31d256509d1db84d
Shah Zobair <shah-zobair@users.noreply.github.com>
2016-09-22 16:46:28 -0400

    Create setupEndecaUser.sh

---
+++ scripts/setupEndecaUser.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#echo 'Adding grpEndeca'
+
+groupadd grpEndeca
+
+echo 'Adding endeca user'
+#useradd endeca
+#passwd endeca
+#useradd -p $(openssl passwd -1 endeca) -g grpEndeca endeca2
+
+useradd -p $(openssl passwd -1 endeca) -g grpEndeca endeca
+
+
+if [ ! -z "$1" ]; then
+        echo "endeca    ALL=(ALL)       ALL" >> $1
+else
+        export EDITOR=$0
+        visudo
+fi
